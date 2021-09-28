b48404414595e45e6775a374f82f00027b43cf30
ttaha <mustafatahacilbas@gmail.com>
2021-09-28 14:36:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //sysout ctrl space
+		System.out.println("Mertcanýn Hayatý");
+		System.out.println("Sabah kalkýþ-07:01");
+		System.out.println("Yüzünü yýkama-07:30");
+		System.out.println("Kahvaltý-08:00");
+		System.out.println("Okula gidiþ-08:30");
+		System.out.println("Agalarla sohbet-09:00");
+		System.out.println("Derste telefonla oynama-09:30-16:40");
+		System.out.println("Öðle arasý týkýnma 13:30");
+		System.out.println("eve gidiþ 16:40");
+		System.out.println("yatmak-19:00");
+		System.out.println("yemek yemek-20:00");
+		System.out.println("kýzlara yazmak-00:00");
+		System.out.println("birini unutmak-02:00");
+		System.out.println("kawga-05:00");
+		System.out.println("ayrýlýk-06:00");
+		System.out.println("yenisini bulmak-07:00")
+		
+	}
+
+}
